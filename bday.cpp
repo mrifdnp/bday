5c5ff1ad60d298690aa9a797b50d5489547c84fa
mrifdnp <113992640+mrifdnp@users.noreply.github.com>
2023-03-27 01:20:48 +0700

    Add files via upload

---
+++ bday.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+string ans;
+int main(){
+	//what u should do : just f11 (compile n run it) :)
+	cout<<"To : Alvilovia Zahra";
+	
+	
+	string str ="Loading, Please wait!\n";
+	cout<<"\n\n\t";
+	cout<<str;
+	for(int i=0;i<35;i++){
+		
+	cout<<char(219);
+	Sleep(150);
+	
+	
+	}
+	
+	system("cls");
+	cout<<"To : Alvilovia Zahra";
+	string stri ="Fully loaded,\n";
+	cout<<"\n\n\t";
+	cout<<stri;
+	for(int i=0;i<35;i++){
+		
+	cout<<char(219);
+	
+	
+	
+	}
+	cout<<endl;
+	system("pause");
+	system("cls");
+	
+	sini:
+	cout<<"To : Alvilovia Zahra"<<endl;	
+	cout<<"your birthday is on the Feb 24th ?ans:yes/no"<<endl;
+	cout<<"ans : ";
+	cin>>ans;
+	
+	
+	if(ans=="yes"){ 
+		string str = "Happy birthday to my gorgeous lil sis. How's your day today? do you hv a great feeling?. Sorry if i'm late to give u a present and i don't know what do you want, ehe ^>^,so I bought something that i thought it was cute. Do you like it? I HOpe  T>T. AHHHH my english is bad, but i think you can understand it. \n17y.o. where is your ID, can i see your pict w/ blue bg. it's COOL. why mine is red bg-_-. \nWhat do you want at your age now, i will always support you from here ^.^ may you always be healthy and happy, your all wishes come true. sorry if i may bother you.*still waiting for mie nyemek part2\n"; 
+		for(int i=0;i<str.length();i++){
+		
+		cout<<str[i];
+		Sleep(150);
+		
+		}
+	}else if (ans=="no"){
+		cout<<"then for whom is this letter?"<<endl;
+		system("cls");
+		goto sini;
+		
+	}else {
+	cout<< "wdym?";
+	system("pause");
+	system("cls");
+	goto sini;
+	}
+		cout<<"\n\n\n\t Otanjoubi Omedetou!"<<endl;
+		cout << " /\\_/\\  \n";
+  		cout << "( o.o )  \n";
+  		cout << " > ^ <   \n";
+	
+return 0;
+getch;
+}
